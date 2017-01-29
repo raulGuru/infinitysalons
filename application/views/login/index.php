cc4498738609b8224091f325ef9f964eca836591
guptaPriyaD <bubbly1013@gmail.com>
2017-01-29 12:12:21 +0530

    Changed login page title

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
-        <title>Shedul</title>
+        <title>Infinity Salon</title>
         <meta content="" name="description">
         <meta content="" name="author">
         <link href="/favicon.ico?v=rMMnxJlqdL" rel="shortcut icon">
