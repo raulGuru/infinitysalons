c9e73519d61e15109ea4e6afff63db6233074f59
guptaPriyaD <bubbly1013@gmail.com>
2017-01-31 16:20:56 +0530

    provider setting changes

--- application/controllers/Provider.php
+++ application/controllers/Provider.php
@@ -20,7 +20,7 @@ class Provider extends CI_Controller {
         //$data['taxes'] = $this->getPosTaxes();
         $query = $this->db->get_where("businesstax");
         $data['taxes'] = $query->result_array();
-        
+
         $this->load->layout('provider/business_settings', $data);
     }
 
@@ -45,67 +45,121 @@ class Provider extends CI_Controller {
         $this->load->view('provider/add_cancellations_reason');
     }
 
-    public function deletePosPaymentTypes() {
-        
+    public function newCancellation() {
+        $this->load->view('cancellations/add_cancellation');
     }
 
-    public function getCancellationReasons() {
+    public function getCancellationReasons($id = '') {
         $returnArr['title'] = "Cancellation Reasons";
-        $returnArr['data'] = $this->provider_model->getCancellationReasons();
+        $returnArr['data'] = $this->provider_model->getCancellationReasons($id);
         return $returnArr;
     }
 
     public function editCancellationReasons() {
         $id = $_GET['id'];
 
-        $data['data'] = $this->provider_model->getCancellationReasons($id);
+        $cancellationReasons = $this->provider_model->getCancellationReasons($id);
         $data['mode'] = 'edit';
+        $data['cancellationReasons'] = $cancellationReasons[0];
+//        echo '<pre>';print_r($data); //die;
 
-        $this->load->view('provider/add_cancellations_reason');
+        $this->load->view('cancellations/add_cancellation', $data);
+    }
+
+    public function addCancellationReasons() {
+        $referral_source = $_POST['referral_source'];
+        $referral_id = $_POST['referral_id'];
+
+        $values = array(
+//            'id' => $referral_id,
+            'referralname' => $referral_source['name'],
+            'active' => $referral_source['active'],
+            'date' => date('Y-m-d H:i:s'),
+        );
+
+        if (!empty($referral_id)) {
+            $q = $this->provider_model->updateReferralSources($referral_id, $values);
+        } else {
+            $q = $this->provider_model->insertReferralSources($values);
+        }
+
+        if ($q == 'true') {
+            $res['success'] = true;
+        } else {
+            $res['error'] = $q;
+        }
+        echo json_encode($res);
     }
 
     public function deleteCancellationReason() {
         
     }
 
-    public function getReferralSources() {
+    public function newReferral() {
+        $this->load->view('referrals/add_referral');
+    }
+
+    public function getReferralSources($id = '') {
         $returnArr['title'] = "Referral Sources";
-        $returnArr['data'] = $this->provider_model->getReferralSources();
+        $returnArr['data'] = $this->provider_model->getReferralSources($id);
         return $returnArr;
     }
 
     public function editReferralSources() {
         $id = $_GET['id'];
 
-        $data['data'] = $this->provider_model->getCancellationReasons($id);
+        $referralSources = $this->provider_model->getReferralSources($id);
         $data['mode'] = 'edit';
-
-        $this->load->view('provider/add_cancellations_reason');
+        $data['referralSources'] = $referralSources[0];
+//        echo '<pre>';print_r($data); die;
+        $this->load->view('referrals/add_referral', $data);
     }
 
-    public function deleteReferralSources() {
-        
+    public function addReferralSources() {
+        $referral_source = $_POST['referral_source'];
+        $referral_id = $_POST['referral_id'];
+
+        $values = array(
+//            'id' => $referral_id,
+            'referralname' => $referral_source['name'],
+            'active' => $referral_source['active'],
+            'date' => date('Y-m-d H:i:s'),
+        );
+
+        if (!empty($referral_id)) {
+            $q = $this->provider_model->updateReferralSources($referral_id, $values);
+        } else {
+            $q = $this->provider_model->insertReferralSources($values);
+        }
+
+        if ($q == 'true') {
+            $res['success'] = true;
+        } else {
+            $res['error'] = $q;
+        }
+        $res['success'] = true;
+        echo json_encode($res);
     }
 
     public function newTax() {
-        $data=array();
-        if(!empty($_GET['id'])) {
+        $data = array();
+        if (!empty($_GET['id'])) {
             $query = $this->db->get_where('businesstax', array('id' => $_GET['id']));
             $data['tax'] = $query->row_array();
         }
         $this->load->view('pointsale/add_tax', $data);
     }
-    
+
     public function addTax() {
         $tax = array(
             'taxname' => $_POST['name'],
             'rate' => $_POST['rate']
         );
-        if(!empty($_POST['tax_id'])) {
+        if (!empty($_POST['tax_id'])) {
             $this->db->update('businesstax', $tax, array('id' => $_POST['tax_id']));
-        }else {
+        } else {
             $this->db->insert('businesstax', $tax);
-        }        
+        }
         $res['success'] = true;
         echo json_encode($res);
     }
--- application/views/cancellations/add_cancellation.php
+++ application/views/cancellations/add_cancellation.php
@@ -1,8 +1,36 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div aria-hidden="true" aria-labelledby="cancellationModal" class="modal slide-up in" id="newCancellationModal" role="dialog" tabindex="-1" style="display: block;">
+    <div class="modal-dialog">
+        <div class="modal-content-wrapper">
+            <div class="modal-content">
+                <div class="modal-header clearfix text-center">
+                    <button aria-hidden="true" class="close" data-dismiss="modal" type="button">
+                        <i class="pg-close fs-14"></i>
+                    </button>
+                    <h4>
+                        <?php echo (!empty($cancellationReasons['id'])) ? "Edit Cancellation Reason" : "New Cancellation Reason" ?>
+                    </h4>
+                </div>
+                <form novalidate="novalidate" class="simple_form new_cancellation_reason" id="new_cancellation_reason" action="/cancellation_reasons" accept-charset="UTF-8" data-remote="true" method="post">
+                    <div class="modal-body sm-padding-10">
+                        <div class="form-group">
+                            <div class="form-group string required cancellation_reason_name">
+                                <label class="string required control-label" for="cancellation_reason_name">
+                                    <abbr title="required">*</abbr> Name
+                                </label>
+                                <input class="string required form-control" placeholder="e.g. Client unavailable" type="text" name="cancellation_reason[name]" id="cancellation_reason_name" value="<?php echo (!empty($cancellationReasons['cancelreason'])) ? $cancellationReasons['cancelreason'] : "" ?>">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="pull-left">
+                        </div>
+                        <button aria-hidden="true" class="btn btn-default" data-dismiss="modal" type="button">
+                            Cancel
+                        </button>
+                        <input type="submit" name="commit" value="Save" class="btn btn-success">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
