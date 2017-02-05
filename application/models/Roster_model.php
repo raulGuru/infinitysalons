58c48779239bfa421ef2b9499f42cb09e519f4b9
guptaPriyaD <bubbly1013@gmail.com>
2017-02-05 13:36:39 +0530

    Added module Roster for staff working days

---
+++ application/models/Roster_model.php
@@ -0,0 +1,55 @@
+<?php
+
+class Roster_model extends CI_Model {
+
+    protected $user_id;
+
+    function __construct() {
+        parent::__construct();
+        $this->user_id = $this->config->item('user_id');
+    }
+
+    public function getUserById($id = '') {
+//        $this->db->select('*');
+        if (isset($id) && $id != '') {
+            $this->db->where('staff.id', $id);
+        }
+        $this->db->from('staff');
+        $this->db->join('roster', 'staff.id = roster.staffid', 'left');
+//        $this->db->get_compiled_select();
+        $result = $this->db->get();
+        $returnArr = $result->result_array();
+
+        return $returnArr;
+    }
+
+    public function updateAllRoster($id) {
+        if (!empty($id)) {
+            $columnArr = array(
+                'sunday' => '0',
+                'monday' => '0',
+                'tuesday' => '0',
+                'wednesday' => '0',
+                'thursday' => '0',
+                'friday' => '0',
+                'saturday' => '0'
+            );
+            if ($this->db->update('roster', $columnArr, array('staffid' => $id))) {
+                return 'true';
+            } else {
+                return 'false';
+            }
+        }
+    }
+
+    public function updateStaffRoster($id, $rosterColumns) {
+        if (!empty($id) && !empty($rosterColumns)) {
+            if ($this->db->update('roster', $rosterColumns, array('staffid' => $id))) {
+                return 'true';
+            } else {
+                return 'false';
+            }
+        }
+    }
+
+}
