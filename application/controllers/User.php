505125d8c28389e0357318b22d2945b89aadbc93
raulGuru <rahul.karizmar225@gmail.com>
2017-02-06 11:58:12 +0530

    user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,39 +1,26 @@
 <?php
-
 class User extends CI_Controller {
-
     function __construct() {
         parent::__construct();
         $this->load->helper('form');
-
         $this->load->model('user_model');
     }
-
     public function account() {
         $data = $loggedUser = array();
         $loggedUser = $this->session->userdata('salon_user');
-
-//        if (isset($loggedUser)) {
-            $email = $loggedUser['email'];
-            $data['user'] = $this->user_model->getUser($email);
-            
-            $this->load->layout('user/account', $data);
-//        } else {
-//            $this->load->view('login/index');
-//        }
+        $email = $loggedUser['email'];
+        $data['user'] = $this->user_model->getUser($email);
+        $this->load->layout('user/account', $data);
     }
-
     public function saveAccount() {
         $updateArr = array();
         $id = $this->input->post('id');
         $updateArr['first_name'] = $this->input->post('first_name');
         $updateArr['last_name'] = $this->input->post('last_name');
         $updateArr['mobile'] = $this->input->post('mobile');
-
         if (!empty($this->input->post('password')) && ($this->input->post('password') == $this->input->post('password_confirmation'))) {
             $updateArr['current_password'] = $this->input->post('password');
         }
-
         if (isset($updateArr) && !empty($updateArr)) {
             $returnArr = $this->user_model->update($id, $updateArr);
         }
@@ -41,5 +28,26 @@ class User extends CI_Controller {
             redirect('user/account');
         }
     }
-
+    public function permissions() {
+        $data['users'] = $this->user_model->getUserById();
+        $this->load->layout('user/permissions', $data);
+    }
+    public function updatePermissions() {
+        if (!empty($_POST['userid']) && !empty($_POST['permissions'] && $_POST['commit'] == 'Save Changes')) {
+            $userid = $_POST['userid'];
+            $permissionColumns = $_POST['permissions'];
+            $updateAll = $this->user_model->updateAllPermissions($userid);
+            if ($updateAll == 'true') {
+                $updateUserPerms = $this->user_model->updateUserPermissions($userid, $permissionColumns);
+                $this->session->set_flashdata('user_perms_update_mesg', 'User permission information has been successfully updated.');
+            } else {
+                $this->session->set_flashdata('user_perms_update_mesg', 'Something went wrong!');
+            }
+        }
+        redirect('user/permissions');
+    }
+    public function getUserPermissions() {
+        $u = $this->user_model->getUserById($_GET['userid']);
+        echo json_encode($u[0]);
+    }
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,30 +1,22 @@
 <?php
-
 class User_model extends CI_Model {
-
     protected $user_id;
-
     function __construct() {
         parent::__construct();
         $this->user_id = $this->config->item('user_id');
     }
-
     public function getUser($email) {
         $returnArr = array();
         $ql = $this->db->get_where('user', array('email' => $email));
         $returnArr = $ql->row_array();
-
         if ($ql->num_rows() > 0) {
             return $returnArr;
         }
     }
-
     public function update($id, $updateArr) {
         $returnArr = array();
-
         $query = $this->db->get_where('user', array('id' => $id, 'email' => $updateArr['email']));
         $email = $query->result_array();
-
         if (isset($email) && !empty($email) && ($email[0]['email'] == $updateArr['email'])) {
             $returnArr['error'] = 'Email is not unique';
         } else {
@@ -32,10 +24,48 @@ class User_model extends CI_Model {
             $this->db->update('user', $updateArr);
             $returnArr['error'] = '';
         }
-
         return $returnArr;
     }
-
+    public function getUserById($id = '') {
+        $this->db->select('user.id, user.first_name, user.last_name, user.email, user.last_login_on, user.mobile, user.session_id, user.status, user.updated_on, userpermissions.userid, userpermissions.home, userpermissions.calender, userpermissions.clients, userpermissions.services, userpermissions.products, userpermissions.discounts, userpermissions.staff, userpermissions.businesssetting, userpermissions.roster');
+        if (isset($id) && $id != '') {
+            $this->db->where('user.id', $id);
+        }
+        $this->db->from('user');
+        $this->db->join('userpermissions', 'user.id = userpermissions.userid', 'left');
+//        $this->db->get_compiled_select();
+        $result = $this->db->get();
+        $returnArr = $result->result_array();
+        return $returnArr;
+    }
+    public function updateAllPermissions($id) {
+        if (!empty($id)) {
+            $columnArr = array(
+                'home' => '0',
+                'calender' => '0',
+                'clients' => '0',
+                'services' => '0',
+                'products' => '0',
+                'discounts' => '0',
+                'staff' => '0',
+                'businesssetting' => '0',
+                'roster' => '0'
+            );
+            if ($this->db->update('userpermissions', $columnArr, array('userid' => $id))) {
+                return 'true';
+            } else {
+                return 'false';
+            }
+        }
+    }
+    public function updateUserPermissions($id, $permissionCols) {
+        if (!empty($id) && !empty($permissionCols)) {
+            if ($this->db->update('userpermissions', $permissionCols, array('userid' => $id))) {
+                return 'true';
+            } else {
+                return 'false';
+            }
+        }
+    }
 }
-
 ?>
